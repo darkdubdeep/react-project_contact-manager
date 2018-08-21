4a7e10cb92d1e1de47f9a2e2ae73fd1705c966a7
ilnur <ex_citer@list.ru>
2018-08-21 21:00:55 +0300

    'commit'

---
+++ contact-manager/src/components/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>John Doe</h4>
+        <ul>
+          <li>Email: John@gmail.com</li>
+          <li>Phone: 555-88-25</li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default Contact;
