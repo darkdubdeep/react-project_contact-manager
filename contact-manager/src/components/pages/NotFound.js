db029b658d852b03a7e49851c9bfd18d760ef778
ilnur <ex_citer@list.ru>
2018-09-10 22:58:10 +0300

    Added 404 page

---
+++ contact-manager/src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4 text-center">404 Page Not Found</h1>
+      <p className="lead text-center">Sorry, page is not exist</p>
+    </div>
+  );
+};
